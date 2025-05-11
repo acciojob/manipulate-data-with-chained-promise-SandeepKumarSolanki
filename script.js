7f1cf82ac5c823fece67b2034a40836fb6bf9e87
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-11 21:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let promise = new promise((resolve,reject)=> {
+	resolve([1,2,3,4]);
+})
+promise
+.then((data)=> {
+	setTimeout(()=>{
+		document.getElementById('output').innerText = data.filter((item)=> item%2 === 1);
+	},1000)
+	resolve(data);
+})
+.then((data)=> {
+	setTimeout(()=>{
+		let evenNumber = data.filter((item)=> item%2 === 0)
+		document.getElementById('output').innerText = evenNumber.map((item)=>item*2);
+	},2000)
+})
